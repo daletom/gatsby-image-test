5e03eb12197cd35109cdff62f5e714ff00028946
daletom <tom@imgix.com>
2019-12-03 18:07:20 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default IndexPage
 export const fluidImage = graphql`
   fragment fluidImage on File {
     childImageSharp {
-      fluid(maxWidth: 760) {
+      fluid(maxWidth: 300) {
         ...GatsbyImageSharpFluid
       }
     }
