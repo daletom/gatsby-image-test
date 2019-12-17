dc55c3001d17559fec5b67d467c9776afab5dd67
daletom <tom@imgix.com>
2019-12-17 14:04:35 -0800

    add page 2 with imgix react

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Imgix from 'react-imgix'
 
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2, using imgix React</p>
     <Link to="/">Go back to the homepage</Link>
+    <Imgix
+      src="https://tom.imgix.net/one.jpg"
+      sizes="(max-width: 300px) 100vw, 300px"
+    />
+    <Imgix
+      src="https://tom.imgix.net/two.jpg"
+      sizes="(max-width: 300px) 100vw, 300px"
+    />
+    <Imgix
+      src="https://tom.imgix.net/three.jpg"
+      sizes="(max-width: 300px) 100vw, 300px"
+    />
   </Layout>
 )
 
