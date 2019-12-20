7b3dee575d97a3c5e66b803218419c5bed94953f
daletom <tom@imgix.com>
2019-12-19 17:49:45 -0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,7 @@ import Layout from '../components/layout'
 
 const Header = ({ siteTitle }) => (
   <div class="topnav">
-    <a href="https://www.xigmi.com" class="active">
-      Home
-    </a>
+    <a href="https://www.xigmi.com">Home</a>
     <a href="https://variablequality.xigmi.com">Variable Q</a>
     <a href="https://s3.xigmi.com">S3 Bucket</a>
     <a href="https://gatsbyimagetest.netlify.com/"> Gatsby image</a>
