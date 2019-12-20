af194bf6a21f7fbad1e07820fb1fb48df58a078f
daletom <tom@imgix.com>
2019-12-19 17:45:19 -0800

    change header

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div class="topnav">
+    <a href="https://www.xigmi.com" class="active">
+      Home
+    </a>
+    <a href="https://variablequality.xigmi.com">Variable Q</a>
+    <a href="https://s3.xigmi.com">S3 Bucket</a>
+    <a href="https://gatsbyimagetest.netlify.com/"> Gatsby image</a>
+    <a href="https://gatsbyimagetest.netlify.com/page-2/"> ix React</a>
   </div>
 )
 
