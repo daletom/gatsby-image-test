4fcb54d44ade024caa720e5f29babea00504aba1
daletom <tom@imgix.com>
2019-12-20 13:40:16 -0800

    add header resp

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,30 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const Header = ({ siteTitle }) => (
-  <div class="topnav">
-    <a href="https://www.xigmi.com">Home</a>
-    <a href="https://variablequality.xigmi.com">Variable Q</a>
-    <a href="https://s3.xigmi.com">S3 Bucket</a>
-    <a href="https://gatsbyimagetest.netlify.com/"> Gatsby image</a>
-    <a href="https://gatsbyimagetest.netlify.com/page-2/"> ix React</a>
-  </div>
+  <nav class="mobile-menu">
+    <label for="show-menu" class="show-menu">
+      <span>Menu</span>
+      <div class="lines"></div>
+    </label>
+    <input type="checkbox" id="show-menu"></input>
+    <ul id="menu">
+      <li>
+        <a href="https://www.xigmi.com">Home</a>
+      </li>
+      <li>
+        <a href="https://variablequality.xigmi.com">Variable Q</a>
+      </li>
+      <li>
+        <a href="https://s3.xigmi.com">S3 Bucket</a>
+      </li>
+      <li>
+        <a href="https://gatsbyimagetest.netlify.com/"> Gatsby image</a>
+      </li>
+      <li>
+        <a href="https://gatsbyimagetest.netlify.com/page-2"> ix React</a>
+      </li>
+    </ul>
+  </nav>
 )
 
 export default Header
